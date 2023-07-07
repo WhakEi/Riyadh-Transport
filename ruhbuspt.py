4c67d1f3a4142ffeae2ad898073f7eb522cac415
WhakEi <83140809+WhakEi@users.noreply.github.com>
2023-07-07 15:33:13 +0300

    Update to 0.12

--- ruhbuspt.py
+++ ruhbuspt.py
@@ -3,10 +3,11 @@ from tkinter import ttk
 from PIL import ImageTk, Image
 import urllib.request
 import io
+import subprocess
 
 bus_routes = {
     "Bus 9": [
-        "Transportation Center A", "Al Batha 210", "Al Batha 208", "Al Batha 207", "Al Batha 206", "Al Batha 205", "Al Batha 204", "Al Batha 202", "Al Batha 201", "Al-Muraba 610", "Al-Muraba 609", "Al-Muraba 607", "Al-Muraba 606", "Al-Muraba 605", "Al-Olaya 613", "Olaya 218", "Olaya 217", "Olaya 216", "Olaya 215", "Olaya 212", "Olaya 211", "Olaya 210", "Olaya 209", "Olaya 208", "Olaya 207", "Olaya 206", "Al-Mughera Bin Shoubah 101", "Al-Murooj 602", "Olaya 106", "Olaya 107", "Olaya 108", "Olaya 109", "Olaya 110", "Olaya 111", "Olaya 113", "Olaya 114", "Olaya 116", "Olaya 117", "Olaya 118", "Al-Olaya 512", "Al-Olaya 513", "Al-Muraba 505", "Al-Muraba 506", "Al-Muraba 508", "Al-Muraba 510"," Al Batha 101"," Al Batha 102"," Al Batha 104"," Al Batha 105"," Al Batha 106"," Al Batha 107"," Al Batha 108"," Al Batha 109", "Transportation Center A"
+        "Transportation Center A", "Al Batha 210", "Al Batha 208", "Al Batha 207", "Al Batha 206", "Al Batha 205", "Al Batha 204", "Al Batha 202", "Al Batha 201", "Al-Muraba 610", "Al-Muraba 609", "Al-Muraba 607", "Al-Muraba 606", "Al-Muraba 605", "Al-Olaya 613", "Olaya 218", "Olaya 217", "Olaya 216", "Olaya 215", "Olaya 212", "Olaya 211", "Olaya 210", "Olaya 209", "Olaya 208", "Olaya 207", "Olaya 206", "Al-Mughera Bin Shoubah 101", "Al-Murooj 602", "Olaya 106", "Olaya 107", "Olaya 108", "Olaya 109", "Olaya 110", "Olaya 111", "Olaya 113", "Olaya 114", "Olaya 116", "Olaya 117", "Olaya 118", "Al-Olaya 512", "Al-Olaya 513", "Al-Muraba 505", "Al-Muraba 506", "Al-Muraba 508", "Al-Muraba 510", "Al Batha 101", "Al Batha 102", "Al Batha 104", "Al Batha 105", "Al Batha 106", "Al Batha 107", "Al Batha 108", "Al Batha 109", "Transportation Center A"
     ],
     "Bus 7": [
         'Transportation Center A', 'Al Batha 210', 'Al Batha 208', 'Al Batha 207', 'Al Batha 206', 'Al Batha 205', 'Al Batha 204', 'Al Batha 202', 'Al Batha 201', 'Al-Muraba 610', 'Al Washm 401', 'An-Namuthajiyah 603', 'An-Namuthajiyah 602', 'King Saud 401', 'An-Nasiriyah 601', 'Ash-Sharafiyah 604', 'Ash-Sharafiyah 603', 'Ash-Sharafiyah 602', 'Ash-Sharafiyah 601', 'King Khalid 206', 'King Khalid 205', 'King Khalid 204', 'King Khalid 203', 'Umm Al Hamam 208', 'Umm Al Hamam 207', 'Umm Al Hamam 206', 'Umm Al Hamam 205', 'Umm Al Hamam 204', 'Umm Al Hamam 203',' Umm Al Hamam 202',' Umm Al Hamam 201',' Al Urubah 404',' Al Urubah 403',' Al Urubah 401', 'King Khalid 101',' Al Urubah302',' Al Urubah303',' Umm Al Hamam101',' Umm Al Hamam102',' Umm Al Hamam103',' Umm Al Hamam104',' Umm Al Hamam105',' Umm Al Hamam106',' Umm Al Hamam107',' Umm Al Hamam108',' King Khalid 103',' King Khalid 104',' King Khalid 105',' King Khalid 106',' Ash-Sharafiyah 501',' Ash-Sharafiyah 502',' Ash-Sharafiyah 503',' Ash-Sharafiyah 504',' An-Nasiriyah 501',' An-Nasiriyah 502',' King Saud 301',' An-Namuthajiyah 501',' An-Namuthajiyah 503',' Al Washm 301',' Al-Muraba 510',' Al Batha 101',' Al Batha 102',' Al Batha 104',' Al Batha 105',' Al Batha 106',' Al Batha 107',' Al Batha 108', 'Transportation Center A'
@@ -42,8 +43,71 @@ bus_routes = {
         'Khalid Bin Al Walid 207', 'Khalid Bin Al Walid 206', 'Al-Hamra 602', 'Al-Hamra 601', 'Al-Ezdihar 604', 'Al-Ezdihar 602', 'Al-Nuzha 602', 'Al-Nuzha 601', 'Al-Maseef 601', 'Al-Murooj 603', 'Al-Murooj 602', 'Al-Murooj 601', 'Al-Nakheel 607', 'Al-Nakheel 606', 'Al-Muhammadiyah 502', 'King Abdullah 405', 'King Abdullah 404', 'KSU 605', 'KSU 606', 'KSU 506', 'KSU 505', 'King Abdullah 304', 'King Abdullah 305', 'King Abdullah 307', 'Al-Muhammadiyah 601', 'Al-Nakheel 507', 'Al-Murooj 501', 'Al-Murooj 503', 'Al-Maseef 501', 'Al-Maseef 502', 'Al-Nuzha 501', 'Al-Ezdihar 504', 'Khalid Bin Al Walid 105', 'Khalid Bin Al Walid 107'
     ],
     "Bus 350": [
-        'KSU 506', 'KSU 507', 'KSU 508', 'KSU 503', 'KSU 504', 'King Abdullah 304', 'King Abdullah 305', 'King Abdullah 307', 'Ar-Raed 502', 'Ar-Raed 503', 'Ar-Raed 504', 'North Al-Mathar 501', 'North Al-Mathar 502', 'North Al-Mathar 503', 'North Al-Mathar 504', 'Al-Mathar 501', 'Al-Mathar 503', 'King Khalid 106', 'King Saud 301', 'An-Namuthajiyah 501', 'An-Namuthajiyah 503', 'Amr Bin Al Aas 307', 'Nasseriya 303', 'Nasseriya 304', 'King Abdulaziz 215', 'Omar Bin Al Khatab 302', 'Omar Bin Al Khatab 303', 'Omar Bin Al Khatab 304', 'Omar Bin Al Khatab 305', 'Omar Bin Al Khatab 306', 'Omar Bin Al Khatab 309', 'Al-Jazeerah 505', 'Al-Jazeerah 506', 'Haroon Rasheed 205', 'Haroon Rasheed 201']
+        'KSU 506', 'KSU 507', 'KSU 508', 'KSU 503', 'KSU 504', 'King Abdullah 304', 'King Abdullah 305', 'King Abdullah 307', 'Ar-Raed 502', 'Ar-Raed 503', 'Ar-Raed 504', 'North Al-Mathar 501', 'North Al-Mathar 502', 'North Al-Mathar 503', 'North Al-Mathar 504', 'Al-Mathar 501', 'Al-Mathar 503', 'King Khalid 106', 'King Saud 301', 'An-Namuthajiyah 501', 'An-Namuthajiyah 503', 'Amr Bin Al Aas 307', 'Nasseriya 303', 'Nasseriya 304', 'King Abdulaziz 215', 'Omar Bin Al Khatab 302', 'Omar Bin Al Khatab 303', 'Omar Bin Al Khatab 304', 'Omar Bin Al Khatab 305', 'Omar Bin Al Khatab 306', 'Omar Bin Al Khatab 309', 'Al-Jazeerah 505', 'Al-Jazeerah 506', 'Haroon Rasheed 205', 'Haroon Rasheed 201'
+    ],
+    "Bus 250": [
+        'As-Sulaimanyah 608', 'King Abdulaziz 112', 'Makkah Al Mukarramah 302', 'Makkah Al Mukarramah 303', 'Khurais 301', 'Khurais 302', 'Khurais 303', 'Khurais 304', 'Khurais 305', 'Khurais 309', 'Ar-Rimayah 502', 'Ar-Rimayah 602', 'Ar-Rimayah 601', 'Khurais 409', 'Khurais 403', 'Khurais 402', 'Khurais 401', 'Makkah Al Mukarramah 403', 'Makkah Al Mukarramah 402', 'King Abdulaziz 212', 'As-Sulaimanyah 608'
+    ],
+    "Bus 730": [
+        'Bilal Bin Rabah 418', 'Western Ring 202', 'Western Ring 201', 'Al Rabeya 401', 'Jeddah 313', 'Jeddah 314', 'King Khalid 202', 'King Abdullah 301', 'King Abdullah 302', 'King Abdullah 304', 'Takhassusi 207', 'Takhassusi 206', 'Takhassusi 205', 'Takhassusi 204', 'Takhassusi 202', 'Takhassusi 201', 'Ath Thumamah 301', 'Ath Thumamah 302', 'Ath Thumamah 303', 'King Abdulaziz 108', 'King Abdulaziz 210', 'King Abdulaziz 209', 'King Abdulaziz 208', 'Ath Thumamah 403', 'Ath Thumamah 402', 'Ath Thumamah 401', 'Takhassusi 101', 'Takhassusi 103', 'Takhassusi 105', 'Takhassusi 106', 'Takhassusi 107', 'King Abdullah 405', 'King Abdullah 404', 'King Abdullah 403', 'King Abdullah 402', 'Al Rabeya 401', 'Western Ring 101', 'Western Ring 102', 'Bilal Bin Rabah 318', 'West Al-Uraija 501']
 }
+
+station_coordinates = {
+    "Al Batha 210": (24.596847986354913, 46.7357939104847),
+    "Al Batha 208": (24.60249830094673, 46.73284766630303),
+    "Transportation Center A": (24.596952083914562, 46.74694745281195),
+    "Al Batha 207": (24.60878792759858, 46.72970453462697),
+    "Al Batha 206": (24.617185415698682, 46.72586919619911),
+    "Al Batha 205": (24.62577359052572, 46.72103250281936),
+    "Al Batha 204": (24.62763640631511, 46.71917641412297),
+    "Al Batha 202": (24.635096995318957, 46.717267152813335),
+    "Al Batha 201": (24.644102898199257, 46.71643990283913),
+    "Al-Muraba 610": (24.652418923534317, 46.71278002327253),
+    "Al-Muraba 609": (24.655548467071373, 46.71175683114123),
+    "Al-Muraba 607": (24.659876049390746, 46.71026139650801),
+    "Al-Muraba 606": (24.665714653408948, 46.707772152917215),
+    "Al-Muraba 605": (24.669505457888064, 46.706217687934426),
+    "Al-Olaya 613": (24.68069938704901, 46.701327750960154),
+    "Olaya 218": (24.684018538928047, 46.690491554669954),
+    "Olaya 217": (24.689116892515276, 46.68760549770582),
+    "Olaya 216": (24.697421985640712, 46.683431980492735),
+    "Olaya 215": (24.702798336592284, 46.68073203387055),
+    "Olaya 212": (24.71212484355953, 46.67598051837226),
+    "Olaya 211": (24.716978399529115, 46.67354507258539),
+    "Olaya 210": (24.725252070017287, 46.669368645289055),
+    "Olaya 209": (24.73047603390432, 46.66683623771702),
+    "Olaya 208": (24.73400361931709, 46.6649801490606),
+    "Olaya 207": (24.736303317263612, 46.66379997709606),
+    "Olaya 206": (24.742348305179117, 46.6607311800559),
+    "Al-Mughera Bin Shoubah 101": (24.754172308981364, 46.664054410490294),
+    "Al-Murooj 602": (24.750063434440168, 46.667299650962505),
+    "Olaya 106": (24.742467429316076, 46.66037156630781),
+    "Olaya 107": (24.73599730500528, 46.66369750553665),
+    "Olaya 108": (24.732880529606323, 46.66521015788421),
+    "Olaya 109": (24.73068796159733, 46.666229397309884),
+    "Olaya 110": (24.724879905803036, 46.669233471440464),
+    "Olaya 111": (24.71767792986553, 46.67284908920229),
+    "Olaya 113": (24.71102939569033, 46.676218113157454),
+    "Olaya 114": (24.70457849373193, 46.679554123979365),
+    "Olaya 116": (24.697219305419697, 46.683111784582664),
+    "Olaya 117": (24.68889339682271, 46.68736610447823),
+    "Olaya 118": (24.683325328877253, 46.69027582932767),
+    "Al-Olaya 512": (24.682138972590412, 46.70010406630585),
+    "Al-Olaya 513": (24.678490798233987, 46.701902212564676),
+    "Al-Muraba 505": (24.671039612926858, 46.705569068160244),
+    "Al-Muraba 506": (24.665969769872834, 46.707242766586226),
+    "Al-Muraba 508": (24.65767233224662, 46.710525790442944),
+    "Al-Muraba 510": (24.652211907800158, 46.71274665953214),
+    "Al Batha 101": (24.642162808753348, 46.71703152006325),
+    "Al Batha 102": (24.634205122452762, 46.71648434937824),
+    "Al Batha 104": (24.62754651585229, 46.71895079685936),
+    "Al Batha 105": (24.62499123077584, 46.721289683131744),
+    "Al Batha 106": (24.61641050710476, 46.725743727830654),
+    "Al Batha 107": (24.61024801046009, 46.72842056226863),
+    "Al Batha 108": (24.602554617927648, 46.73239458780619),
+    "Al Batha 109": (24.598771809398723, 46.73437452715397)
+}
+
 
 bus_images = {
     # Replace these URLs with the actual image URLs for each bus route
@@ -55,8 +119,10 @@ bus_images = {
     "Bus 17": "https://i.imgur.com/ykrD7e3.png",
     "Bus 160": "https://i.imgur.com/d7nj24n.jpg",
     "Bus 660": "https://i.imgur.com/I4DwMuQ.png",
-    "Bus 680": "https://i.imgur.com/dgJGtd6.png"A
+    "Bus 680": "https://i.imgur.com/dgJGtd6.png"
 }
+
+bus_stations = bus_routes
 
 def find_route():
     start = start_entry.get()
@@ -75,7 +141,7 @@ def find_route():
                     if bus1 == bus2:
                         time = abs(bus_routes[bus1].index(start) - bus_routes[bus2].index(destination))
                         cost = len(set([bus1]))
-                        route = f"{bus1} to station {destination} ({time} stops) - Alpha v0.10"
+                        route = f"{bus1} to station {destination} ({time} stops) - Alpha v0.12"
 
                         if time < fastest_time:
                             fastest_time = time
@@ -90,7 +156,7 @@ def find_route():
                             time2 = abs(bus_routes[bus2].index(intersect) - bus_routes[bus2].index(destination))
                             time = time1 + time2
                             cost = len(set([bus1,bus2]))
-                            route = f"{bus1} to station {intersect} ({time1} stops), {bus2} to station {destination} ({time2} stops) - Alpha v0.10"
+                            route = f"{bus1} to station {intersect} ({time1} stops), {bus2} to station {destination} ({time2} stops) - Alpha v0.12"
 
                             if time < fastest_time:
                                 fastest_time = time
@@ -107,7 +173,7 @@ def find_route():
                                     time3 = abs(bus_routes[bus2].index(intersect2) - bus_routes[bus2].index(destination))
                                     time = time1 + time2 + time3
                                     cost = len(set([bus1,bus2,bus3]))
-                                    route = f"{bus1} to station {intersect} ({time1} stops), {bus3} to station {intersect2} ({time2} stops), {bus2} to station {destination} ({time3} stops) - Alpha v0.10"
+                                    route = f"{bus1} to station {intersect} ({time1} stops), {bus3} to station {intersect2} ({time2} stops), {bus2} to station {destination} ({time3} stops) - Alpha v0.12"
 
                                     if time < fastest_time:
                                         fastest_time = time
@@ -126,7 +192,7 @@ def find_route():
                                             time4 = abs(bus_routes[bus2].index(intersect3) - bus_routes[bus2].index(destination))
                                             time = time1 + time2 + time3 + time4
                                             cost = len(set([bus1,bus2,bus3,bus4]))
-                                            route = f"{bus1} to station {intersect} ({time1} stops), {bus3} to station {intersect2} ({time2} stops), {bus4} to station {intersect3} ({time3} stops), {bus2} to station {destination} ({time4} stops) - Alpha v0.10"
+                                            route = f"{bus1} to station {intersect} ({time1} stops), {bus3} to station {intersect2} ({time2} stops), {bus4} to station {intersect3} ({time3} stops), {bus2} to station {destination} ({time4} stops) - Alpha v0.12"
 
                                             if time < fastest_time:
                                                 fastest_time = time
@@ -179,8 +245,16 @@ button.pack()
 result_label = tk.Label(route_finder_tab, text="")
 result_label.pack()
 
+def on_button_click():
+    subprocess.Popen(['python', 'map.py'])
+
+# ...
+
 bus_info_tab = ttk.Frame(tab_control)
 tab_control.add(bus_info_tab, text="Bus Information")
+
+button = tk.Button(bus_info_tab, text="View Bus 9 route", command=on_button_click)
+button.pack()
 
 bus_select_label = tk.Label(bus_info_tab, text="Select a bus:")
 bus_select_label.pack()
@@ -203,5 +277,6 @@ route_image = tk.Label(bus_info_tab)
 route_image.pack()
 
 tab_control.pack(expand=1, fill="both")
+
 
 root.mainloop()
